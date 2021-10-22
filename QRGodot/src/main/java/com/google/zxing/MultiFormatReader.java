4962b6ea641906f0f4082c532efe28c78a10eab7
FreeG <freeg@inwind.it>
2021-10-22 18:33:47 +0200

    Removed unused code to reduce library size

--- QRGodot/src/main/java/com/google/zxing/MultiFormatReader.java
+++ QRGodot/src/main/java/com/google/zxing/MultiFormatReader.java
@@ -97,62 +97,17 @@ public final class MultiFormatReader implements Reader {
   public void setHints(Map<DecodeHintType,?> hints) {
     this.hints = hints;
 
-    boolean tryHarder = hints != null && hints.containsKey(DecodeHintType.TRY_HARDER);
     @SuppressWarnings("unchecked")
     Collection<BarcodeFormat> formats =
         hints == null ? null : (Collection<BarcodeFormat>) hints.get(DecodeHintType.POSSIBLE_FORMATS);
     Collection<Reader> readers = new ArrayList<>();
     if (formats != null) {
-      boolean addOneDReader =
-          formats.contains(BarcodeFormat.UPC_A) ||
-          formats.contains(BarcodeFormat.UPC_E) ||
-          formats.contains(BarcodeFormat.EAN_13) ||
-          formats.contains(BarcodeFormat.EAN_8) ||
-          formats.contains(BarcodeFormat.CODABAR) ||
-          formats.contains(BarcodeFormat.CODE_39) ||
-          formats.contains(BarcodeFormat.CODE_93) ||
-          formats.contains(BarcodeFormat.CODE_128) ||
-          formats.contains(BarcodeFormat.ITF) ||
-          formats.contains(BarcodeFormat.RSS_14) ||
-          formats.contains(BarcodeFormat.RSS_EXPANDED);
-      // Put 1D readers upfront in "normal" mode
-      if (addOneDReader && !tryHarder) {
-        readers.add(new MultiFormatOneDReader(hints));
-      }
-      if (formats.contains(BarcodeFormat.QR_CODE)) {
+       if (formats.contains(BarcodeFormat.QR_CODE)) {
         readers.add(new QRCodeReader());
-      }
-      if (formats.contains(BarcodeFormat.DATA_MATRIX)) {
-        readers.add(new DataMatrixReader());
-      }
-      if (formats.contains(BarcodeFormat.AZTEC)) {
-        readers.add(new AztecReader());
-      }
-      if (formats.contains(BarcodeFormat.PDF_417)) {
-         readers.add(new PDF417Reader());
-      }
-      if (formats.contains(BarcodeFormat.MAXICODE)) {
-         readers.add(new MaxiCodeReader());
-      }
-      // At end in "try harder" mode
-      if (addOneDReader && tryHarder) {
-        readers.add(new MultiFormatOneDReader(hints));
       }
     }
     if (readers.isEmpty()) {
-      if (!tryHarder) {
-        readers.add(new MultiFormatOneDReader(hints));
-      }
-
       readers.add(new QRCodeReader());
-      readers.add(new DataMatrixReader());
-      readers.add(new AztecReader());
-      readers.add(new PDF417Reader());
-      readers.add(new MaxiCodeReader());
-
-      if (tryHarder) {
-        readers.add(new MultiFormatOneDReader(hints));
-      }
     }
     this.readers = readers.toArray(EMPTY_READER_ARRAY);
   }
--- QRGodot/src/main/java/com/google/zxing/MultiFormatWriter.java
+++ QRGodot/src/main/java/com/google/zxing/MultiFormatWriter.java
@@ -57,44 +57,8 @@ public final class MultiFormatWriter implements Writer {
 
     Writer writer;
     switch (format) {
-      case EAN_8:
-        writer = new EAN8Writer();
-        break;
-      case UPC_E:
-        writer = new UPCEWriter();
-        break;
-      case EAN_13:
-        writer = new EAN13Writer();
-        break;
-      case UPC_A:
-        writer = new UPCAWriter();
-        break;
       case QR_CODE:
         writer = new QRCodeWriter();
-        break;
-      case CODE_39:
-        writer = new Code39Writer();
-        break;
-      case CODE_93:
-        writer = new Code93Writer();
-        break;
-      case CODE_128:
-        writer = new Code128Writer();
-        break;
-      case ITF:
-        writer = new ITFWriter();
-        break;
-      case PDF_417:
-        writer = new PDF417Writer();
-        break;
-      case CODABAR:
-        writer = new CodaBarWriter();
-        break;
-      case DATA_MATRIX:
-        writer = new DataMatrixWriter();
-        break;
-      case AZTEC:
-        writer = new AztecWriter();
         break;
       default:
         throw new IllegalArgumentException("No encoder available for format " + format);
